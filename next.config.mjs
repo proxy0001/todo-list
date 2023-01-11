67e1b1973dd0f4ff4ab0cb68144a0a585fb21661
proxy0001 <g.proxy0001@gmail.com>
2023-01-12 00:51:55 +0800

    chore: fix css loader for SSR

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,11 @@
  */
 !process.env.SKIP_ENV_VALIDATION && (await import("./src/env/server.mjs"));
 
+/**
+ * transpliePackage for react-spectrum to load css
+ * @see https://react-spectrum.adobe.com/react-spectrum/ssr.html#nextjs
+ */
+
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
@@ -13,60 +18,55 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
+  transpilePackages: [
+    '@adobe/react-spectrum',
+    '@react-spectrum/actiongroup',
+    '@react-spectrum/breadcrumbs',
+    '@react-spectrum/badge',
+    '@react-spectrum/button',
+    '@react-spectrum/buttongroup',
+    '@react-spectrum/calendar',
+    '@react-spectrum/checkbox',
+    '@react-spectrum/combobox',
+    '@react-spectrum/contextualhelp',
+    '@react-spectrum/datepicker',
+    '@react-spectrum/dialog',
+    '@react-spectrum/divider',
+    '@react-spectrum/form',
+    '@react-spectrum/icon',
+    '@react-spectrum/illustratedmessage',
+    '@react-spectrum/image',
+    '@react-spectrum/label',
+    '@react-spectrum/labeledvalue',
+    '@react-spectrum/layout',
+    '@react-spectrum/link',
+    '@react-spectrum/list',
+    '@react-spectrum/listbox',
+    '@react-spectrum/menu',
+    '@react-spectrum/meter',
+    '@react-spectrum/numberfield',
+    '@react-spectrum/overlays',
+    '@react-spectrum/picker',
+    '@react-spectrum/progress',
+    '@react-spectrum/provider',
+    '@react-spectrum/radio',
+    '@react-spectrum/slider',
+    '@react-spectrum/searchfield',
+    '@react-spectrum/statuslight',
+    '@react-spectrum/switch',
+    '@react-spectrum/table',
+    '@react-spectrum/tabs',
+    '@react-spectrum/text',
+    '@react-spectrum/textfield',
+    '@react-spectrum/theme-dark',
+    '@react-spectrum/theme-default',
+    '@react-spectrum/theme-light',
+    '@react-spectrum/tooltip',
+    '@react-spectrum/view',
+    '@react-spectrum/well',
+    '@spectrum-icons/illustrations',
+    '@spectrum-icons/ui',
+    '@spectrum-icons/workflow',
+  ],
 };
-
-/**
- * @see https://react-spectrum.adobe.com/react-spectrum/ssr.html#nextjs
- */
-import withTmInitializer from 'next-transpile-modules';
-const withTM = withTmInitializer([
-  '@adobe/react-spectrum',
-  '@react-spectrum/actiongroup',
-  '@react-spectrum/breadcrumbs',
-  '@react-spectrum/badge',
-  '@react-spectrum/button',
-  '@react-spectrum/buttongroup',
-  '@react-spectrum/calendar',
-  '@react-spectrum/checkbox',
-  '@react-spectrum/combobox',
-  '@react-spectrum/contextualhelp',
-  '@react-spectrum/datepicker',
-  '@react-spectrum/dialog',
-  '@react-spectrum/divider',
-  '@react-spectrum/form',
-  '@react-spectrum/icon',
-  '@react-spectrum/illustratedmessage',
-  '@react-spectrum/image',
-  '@react-spectrum/label',
-  '@react-spectrum/labeledvalue',
-  '@react-spectrum/layout',
-  '@react-spectrum/link',
-  '@react-spectrum/list',
-  '@react-spectrum/listbox',
-  '@react-spectrum/menu',
-  '@react-spectrum/meter',
-  '@react-spectrum/numberfield',
-  '@react-spectrum/overlays',
-  '@react-spectrum/picker',
-  '@react-spectrum/progress',
-  '@react-spectrum/provider',
-  '@react-spectrum/radio',
-  '@react-spectrum/slider',
-  '@react-spectrum/searchfield',
-  '@react-spectrum/statuslight',
-  '@react-spectrum/switch',
-  '@react-spectrum/table',
-  '@react-spectrum/tabs',
-  '@react-spectrum/text',
-  '@react-spectrum/textfield',
-  '@react-spectrum/theme-dark',
-  '@react-spectrum/theme-default',
-  '@react-spectrum/theme-light',
-  '@react-spectrum/tooltip',
-  '@react-spectrum/view',
-  '@react-spectrum/well',
-  // '@spectrum-icons/illustrations',
-  '@spectrum-icons/ui',
-  '@spectrum-icons/workflow'
-]);
-export default withTM(config);
+export default config;
