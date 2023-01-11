f302b1db5242b4992fff9bffb63a2395a913452a
proxy0001 <g.proxy0001@gmail.com>
2023-01-12 00:57:42 +0800

    chore: fix SSR problem for react spectrum

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,25 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-import {defaultTheme, Provider} from '@adobe/react-spectrum';
+import {defaultTheme, SSRProvider, Provider} from '@adobe/react-spectrum';
 import { api } from "../utils/api";
 
 import "../styles/globals.css";
 
+/**
+ * @see https://react-spectrum.adobe.com/react-spectrum/ssr.html#ssr-provider
+ */
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
     <SessionProvider session={session}>
-      <Provider theme={defaultTheme}>
-        <Component {...pageProps} />
-      </Provider>
+      <SSRProvider>
+        <Provider theme={defaultTheme} locale="en-US">
+          <Component {...pageProps} />
+        </Provider>
+      </SSRProvider>
     </SessionProvider>
   );
 };
