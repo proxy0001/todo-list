f1006158e3fe94b6bd62b67903739e8a3e7c139e
proxy0001 <g.proxy0001@gmail.com>
2023-02-01 02:09:23 +0800

    test: setup jest to support both server environment and client environment

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,50 @@
-import nextJest from 'next/jest';
-import type {Config} from 'jest';
+import nextJest from "next/jest";
+import type { Config } from "jest";
 
+/**
+ * setup jest config with Next.js
+ * @see https://nextjs.org/docs/testing#setting-up-jest-with-the-rust-compiler
+ */
 const createJestConfig = nextJest({
-  dir: './',
-})
+  dir: "./",
+});
 
-const customJestConfig: Config = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  moduleDirectories: ['node_modules', '<rootDir>/'],
-  testEnvironment: 'jest-environment-jsdom',
-}
+/**
+ * setup jest config with create-t3-app
+ * @see https://www.youtube.com/watch?v=YRGo1H-qNQs
+ */
+const commonConfig: Config = {
+  clearMocks: true,
+  preset: "ts-jest/presets/js-with-ts",
+  transform: {
+    "^.+\\.mjs$": "ts-jest",
+  },
+};
 
-export default createJestConfig(customJestConfig);
+/**
+ * separate diff configs for server code and client code
+ * main diff is testEnvironment
+ * @see https://stackoverflow.com/questions/41318115/testing-two-environments-with-jest
+ */
+const config: Config = {
+  coverageProvider: "v8",
+  projects: [
+    {
+      ...commonConfig,
+      displayName: 'server',
+      rootDir: "<rootDir>/src/server",
+      testEnvironment: "node",
+    },
+    {
+      ...commonConfig,
+      displayName: 'client',
+      rootDir: "<rootDir>",
+      testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/src/server/"],
+      setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+      moduleDirectories: ["node_modules", "<rootDir>/"],
+      testEnvironment: "jest-environment-jsdom",
+    }
+  ],
+};
+
+export default createJestConfig(config);
